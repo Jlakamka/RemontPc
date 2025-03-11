9f210fde58b43bbd9bc1cc3ff29eb7d3c2817bf6
Jlakamka <selov.dmitriy@mail.ru>
2025-03-11 17:22:36 +0300

    footer create

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/public/components/footer/Footer";
 import Header from "@/public/components/header/Header";
 import Information from "@/public/components/information/Information";
 import Image from "next/image";
@@ -12,6 +13,7 @@ export default function Contacts() {
         repudiandae eum dolores quaerat soluta porro tempora harum recusandae
         qui fugiat vel?
       </p>
+      <Footer />
     </>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import About from "@/public/components/about/About";
+import Footer from "@/public/components/footer/Footer";
 import Header from "@/public/components/header/Header";
 import Information from "@/public/components/information/Information";
 
@@ -9,6 +10,7 @@ export default function Home() {
       <Information />
       <About />
       {/* <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"></div> */}
+      <Footer />
     </>
   );
 }

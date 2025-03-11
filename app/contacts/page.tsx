784b65b5cfb3a05780affa8ef084f1a320f5abdd
Jlakamka <selov.dmitriy@mail.ru>
2025-03-11 16:59:50 +0300

    create contacts

---
+++ app/contacts/page.tsx
@@ -0,0 +1,12 @@
+import Header from "@/public/components/header/Header";
+import Information from "@/public/components/information/Information";
+import Image from "next/image";
+
+export default function Contacts() {
+  return (
+    <>
+      <Header />
+      <Information />
+    </>
+  );
+}
